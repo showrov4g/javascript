fdf0880fb084cb80f87448413cb48657ed0aea7d
Showrov Ghosh <showrovg00@gmail.com>
2021-08-27 00:24:03 +0600

    compare

--- 1/operator.js
+++ 1/operator.js
@@ -32,7 +32,7 @@
 
 // Comparison Operators
 
-var a = 30;
-var b = 25;
+var a = 70;
+var b = 60;
 
-console.log (a != b);
+console.log (a > b);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # javascript
+hello i am Showrov ghosh. now i am start learnign Javascript.
