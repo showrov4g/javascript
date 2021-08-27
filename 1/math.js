20e2f6812e7337864c87706d574d301369b1d8e1
Showrov Ghosh <showrovg00@gmail.com>
2021-08-27 23:43:39 +0600

    round

--- 1/math.js
+++ 1/math.js
@@ -1,8 +1,11 @@
-console.log(Math.PI);
-console.log(Math.E);
-console.log(Math.SQRT2);
-console.log(Math.SQRT1_2);
-console.log(Math.LN2);
-console.log(Math.LN10);
-console.log(Math.LOG2E);
-console.log(Math.LOG10E);
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.SQRT2);
+// console.log(Math.SQRT1_2);
+// console.log(Math.LN2);
+// console.log(Math.LN10);
+// console.log(Math.LOG2E);
+// console.log(Math.LOG10E);
+
+console.log(Math.round(5.897));
+console.log(Math.ceil(5.897));
