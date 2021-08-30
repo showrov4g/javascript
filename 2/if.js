58744473db2afeb6a8e6d955276f738ccf1bbcef
Showrov Ghosh <showrovg00@gmail.com>
2021-08-31 01:49:04 +0600

    if

--- 2/if.js
+++ 2/if.js
@@ -1,6 +1,39 @@
-var a = 5;
-var b =6;
+// var a = 50;
+// var b =10;
 
-if (a < b){
-    console.log("a is greater than b");
+// if (a > b){
+//     console.log(a +' '+ "is greater than" +' '+ b);
+// }
+
+// if ( a < b){
+//     console.log( b + ' is bigger than' + a);
+// }
+
+// var n=50;
+
+// if (n % 4 == 1){
+//     console.log ( n + ' n is odd number');
+// }
+
+// if (n % 4 ==0){
+//     console.log( n + ' n is even number ');
+
+// }
+
+// var hour = 6;
+
+// if (hour < 8){
+//     console.log("good morning");
+// }
+// if (hour > 9){
+//     console.log( "good evening")
+// }
+
+
+var a = 20;
+
+if (a > 10){
+    console.log(a + " is bigger than a")
+}else{
+    console.log(a + " is smoller than a")
 }
