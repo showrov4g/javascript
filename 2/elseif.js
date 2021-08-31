8dc25854e284ff7758e9c48f89a4a9ad6ce00914
showrov4g <showrovg00@gmail.com>
2021-09-01 00:47:38 +0600

    else if

---
+++ 2/elseif.js
@@ -0,0 +1,10 @@
+var a = 40;
+var b = 40;
+
+if(a > b){
+    console.log("A is greater than B");
+}else if ( a < b){
+    console.log('B is greater than A');
+}else{
+    console.log('Both are Equal')
+}
