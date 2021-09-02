8e17f7c5f5d4ed069f31da65ac25293e79b2909b
showrov4g <showrovg00@gmail.com>
2021-09-02 23:52:36 +0600

    push

--- 2/elseif.js
+++ 2/elseif.js
@@ -1,22 +1,35 @@
-var a = 40;
-var b = 40;
+var a = 50;
+var b = 50;
 
-if(a > b){
-    console.log("A is greater than B");
+if ( a > b){
+    console.log(" a is greater than B")
 }else if ( a < b){
-    console.log('B is greater than A');
-}else{
-    console.log('Both are Equal')
-}
-
-var n = 0;
-
-if (n == 0){
-    console.log("n is simple number")
-}
-else if (n%2 == 0){
-    console.log("n is Even number");
+    console.log ("B is greater than A")
 }
 else{
-    console.log("n is odd number")
+    console.log( "both are requal")
 }
+
+var n = 5;
+
+if (n == 0){
+    console.log("N is simple Number")
+}
+else if(n % 2 == 0){
+    console.log("N is Even Number")
+}
+else("N is odd number");
+
+
+
+// var n = 0;
+
+// if (n == 0){
+//     console.log("n is simple number")
+// }
+// else if (n%2 == 0){
+//     console.log("n is Even number");
+// }
+// else{
+//     console.log("n is odd number")
+// }
