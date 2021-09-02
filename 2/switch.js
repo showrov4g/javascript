f33df486ecbea2aa2875b04f0b09f0e99887e629
showrov4g <showrovg00@gmail.com>
2021-09-03 00:22:05 +0600

    switch

---
+++ 2/switch.js
@@ -0,0 +1,27 @@
+// var Date= new Date();
+
+var today = date.getDay();
+
+switch(today){
+    case 0:
+        console.log("this is satarday")
+    break
+    case 1:
+        console.log("this is Sunday")
+    break
+    case 2:
+        console.log("this is Monday")
+    break
+    case 3:
+        console.log("this is Tuesday")
+    break
+    case 4:
+        console.log("this is Wednessday")
+    break
+    case 5:
+        console.log("This is Thusday")
+    break
+    case 0:
+        console.log("this is Friday")
+    break
+}
