04c42617c686e61b4de5d69d41fea95034a4e955
showrov4g <showrovg00@gmail.com>
2021-09-09 01:46:30 +0600

    put

--- 3/for.js
+++ 3/for.js
@@ -1,11 +1,16 @@
 // for( var i = 0; i< 1000; i-= 100){
 //     console.log( (i+1) + 'showrov ghosh')
 // }
-var sum = 0;
-for (var i = 1; i <= 10; i++){
+// var sum = 0;
+// for (var i = 1; i <= 10; i++){
 
-    console.log(sum + '+' + i + '=' + (sum+i))
-    sum += i;
+//     console.log(sum + '+' + i + '=' + (sum+i))
+//     sum += i;
+// }
+
+// console.log('result ='+ sum)
+
+for(var i = 2 ; i <=10 ; i++){
+    console.log(i)
 }
 
-console.log('result ='+ sum)
