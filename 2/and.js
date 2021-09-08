f9207a85e1188514a8eaff4081f974c6b752e4a6
showrov4g <showrovg00@gmail.com>
2021-09-09 00:48:14 +0600

    loop

---
+++ 2/and.js
@@ -0,0 +1,11 @@
+var Name= '';
+
+var fullName = ''
+
+if (Name.length == 0){
+    fullName = 'Showrov ghosh'
+}else{
+    fullName = Name
+}
+
+console.log (fullName)
--- 2/tuerny.js
+++ 2/tuerny.js
@@ -1,9 +1,14 @@
-var a = 10;
+var n = 10;
 var str = '';
 
-if ( a % 2 == 0){
+if ( n % 2 == 0){
     str = 'even'
 }else{
     str = 'odd'
 }
 console.log(str);
+
+var result = n % 2 == 0 ? "even" : "odd"
+
+console.log(result);
+
