3c02112b201ca27589d6051584459b6e7022f0e8
showrov4g <showrovg00@gmail.com>
2021-09-11 23:38:32 +0600

    a

--- 3/for.js
+++ 3/for.js
@@ -1,16 +1,7 @@
-// for( var i = 0; i< 1000; i-= 100){
-//     console.log( (i+1) + 'showrov ghosh')
-// }
-// var sum = 0;
-// for (var i = 1; i <= 10; i++){
-
-//     console.log(sum + '+' + i + '=' + (sum+i))
-//     sum += i;
-// }
-
-// console.log('result ='+ sum)
-
-for(var i = 2 ; i <=10 ; i++){
-    console.log(i)
+for(var i = 0; i <= 10 ; i++){
+    console.log((i+1) + "Showrov ghosh")
 }
+// for(var i = 2 ; i <=10 ; i++){
+//     console.log(i)
+// }
 
