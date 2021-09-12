6274e3032e3b20034fa66ecf48dc505176d17304
showrov4g <showrovg00@gmail.com>
2021-09-13 00:50:08 +0600

    odi

--- w3school/1/for.js
+++ w3school/1/for.js
@@ -1,3 +1,9 @@
-let text= "";
+const cars=['volovo', 'odi', 'Bugati', 'lambergeni']
+let i = 1;
+let len = cars.length;
+let text= '';
+for( ; i <len; i++){
+    text += cars[i] + "<br>"
+}
+document.getElementById('demo').innerHTML= text;
 
-document.getElementById('demo').innerHTML = text; 
