4a598aeb7a763c626121296cab3884135de05280
showrov4g <showrovg00@gmail.com>
2021-09-13 00:40:50 +0600

    text

--- w3school/1/for.js
+++ w3school/1/for.js
@@ -1,15 +1,3 @@
-const cars =["BMW", "volovo", "Saab", "Ford", "Fiat", "Audi"]
-let text ="";
+let text= "";
 
-for(let i=0; i <cars.length; i++){
-    text += cars[i]+ "<br>";
-}
-document.getElementById('demo').innerHTML= text;
-
-/* <script>
-const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-
-let text = "";
-for (let i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
-} */
+document.getElementById('demo').innerHTML = text; 
