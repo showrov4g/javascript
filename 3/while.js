e35f2e30f0f30708fd07d2143de53210cc51c561
showrov4g <showrovg00@gmail.com>
2021-09-15 00:38:49 +0600

    chicken dinner

--- 3/while.js
+++ 3/while.js
@@ -1,5 +1,17 @@
-var i= 0;
-while( i < 10){
-    console.log("Showrov Ghosh")
-    i++
+// var i= 0;
+// while( i < 10){
+//     console.log("Showrov Ghosh")
+//     i++
+// }
+
+var isRunning = true;
+
+while(isRunning){
+    var  rand= math.floor(math.random() *10 + 1) 
+    if( rand == 9){
+        console.log(" i am particing Javascript");
+        isRunning = false
+    }else{
+        console.log("you have own rand")
+    }
 }
