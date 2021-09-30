87c2e094bfa43672ee0cd99aed2524157d3caea2
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-10-01 01:16:35 +0600

    looping one to 50

--- 3/for.js
+++ 3/for.js
@@ -1,7 +1,11 @@
-for(var i = 0; i <= 10 ; i++){
+for(var i = 0; i <= 100 ; i++){
     console.log((i+1) + "Showrov ghosh")
 }
 // for(var i = 2 ; i <=10 ; i++){
 //     console.log(i)
 // }
+
+for(var j=0; j<=50; j++){
+    console.log("value is ", j)
+}
 
