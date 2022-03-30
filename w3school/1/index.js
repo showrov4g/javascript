60bc1232dfb9bb31216520dcafbd8903db52693c
showrov4g <showrovg00@gmail.com>
2022-03-30 11:27:21 +0600

    main

--- w3school/1/index.js
+++ w3school/1/index.js
@@ -1,11 +1,22 @@
-console.log(typeof(10 + "20"));
-console.log(typeof( 10 - "5"));
-console.log (typeof( "java" + "Script"));
-console.log(typeof( " " + " "));
-console.log(typeof(" " + "5"));
-console.log("Showrov" - "ghosh")
-console.log(true + true);
-console.log( true + false);
-console.log ( false + true);
-console.log( true - false);
-console.log ( false - true);
+// console.log(typeof(10 + "20"));
+// console.log(typeof( 10 - "5"));
+// console.log (typeof( "java" + "Script"));
+// console.log(typeof( " " + " "));
+// console.log(typeof(" " + "5"));
+// console.log("Showrov" - "ghosh")
+// console.log(true + true);
+// console.log( true + false);
+// console.log ( false + true);
+// console.log( true - false);
+// console.log ( false - true);
+
+
+// var iAmUseless = null;
+// console.log(iAmUseless);
+// console.log(typeof(iAmUseless))
+
+var myPhoneNmumber= 547565654;
+var myName = "Showrov ghosh";
+
+console.log(isNaN(myPhoneNmumber));
+console.log(isNaN(myName))
