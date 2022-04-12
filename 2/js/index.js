44515a8a3b0aafb2be8c1a4dacb9b45bf17917d6
showrov4g <showrovg00@gmail.com>
2022-04-12 20:36:07 +0600

    mian

--- 2/js/index.js
+++ 2/js/index.js
@@ -5,3 +5,27 @@
 //     document.write("showrov");
 //     counter++;
 // }
+
+// let counter = 1;
+// let sum = 0;
+// for (let counter = 1; counter <= 10; counter++) {
+//     document.write(counter);
+//     document.write("<br>");
+//      for( let counter2 =1; counter2 < 3; counter2++ ){
+//          document.write("sk");
+//          document.write("<br>");
+//      }
+// }
+
+// let counter = 1;
+// let sum = 0;
+// for (let counter = 1; counter <= 10; counter++){
+//     document.write(counter);
+//     document.write("<br>");
+//     for (let counter2 = 1; counter2 < 3; counter2++){
+//         document.write("sk");
+//         document.write("<br>");
+//     }
+// }
+
+prompt("enter your age");
