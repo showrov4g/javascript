6a3615a6bdb1e6d20a6b7e07e57124cc1569b248
showrov4g <showrovg00@gmail.com>
2022-04-17 23:36:23 -0700

    argumrnt

--- 2/js/index.js
+++ 2/js/index.js
@@ -1,6 +1,13 @@
-console.log("hello world");
+// console.log("hello world");
 
-function add( num1, num2){
-    console.log(num1 + num2);
+// function add( num1, num2){
+//     console.log(num1 + num2);
+// }
+// add( 3 , 9 );
+
+function add(){
+    console.log(arguments.length);
 }
-add( 3 , 9 );
+
+add ( 5 , 6);
+add(6, 4, 5, 7);
