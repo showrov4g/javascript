6a2241eb08b172e16481588a7dbea0dfecc13845
showrov4g <showrovg00@gmail.com>
2022-08-16 12:32:24 +0630

    start

--- variable.js
+++ variable.js
@@ -1,17 +1,7 @@
 // const a = "হরে কৃষ্ণ হরে কৃষ্ণ, কৃষ্ণ কৃষ্ণ, হরে হরে, হরে রাম হরে রাম, রাম রাম হরে হরে  ";
 // console.log (a);
 
-// operator in js 
-console.log("Operator in js");
 
-let a =  40;
-let b = 20;
-console.log("a = 40","b =20");
-console.log("a + b =", (a + b));
-console.log("a - b =", (a - b));
-console.log("a ** b =", (a ** b));
-console.log("a / b =", (a / b));
-console.log("a % b =", (a % b));
-console.log("a ++ =", (a ++));
-console.log("a -- =", (a --));
-console.log("a =", a);
+for (i<0 ; i =5; i++){
+    console.log(i);
+}
