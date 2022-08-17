de9c166a83a79e4c24741367203ea50e56ffc2a7
showrov4g <showrovg00@gmail.com>
2022-08-17 09:48:35 +0600

    loop

--- variable.js
+++ variable.js
@@ -1,4 +1,12 @@
 // const a = "হরে কৃষ্ণ হরে কৃষ্ণ, কৃষ্ণ কৃষ্ণ, হরে হরে, হরে রাম হরে রাম, রাম রাম হরে হরে  ";
 // console.log (a);
 
+// let sum = 0;
+// let n = prompt("inter the valu of r");
+// n = Number.parseInt(n);
 
+
+// for (let i = 0; i < n; i++){
+//     sum += (i+1);
+// }
+// console.log("sum of first" +" "+ n + "natural number is" + sum );
