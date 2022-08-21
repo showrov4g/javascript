65ae3539a41684716a71a8742e6acb68ba058b6b
showrov4g <showrovg00@gmail.com>
2022-08-21 10:39:26 +0600

    templete template laterate

--- variable.js
+++ variable.js
@@ -1,2 +1,9 @@
 let sbc = "Showrov ghosh";
 console.log(sbc);
+
+let dfv = 'i am showrov ghosh. one of the best computer in the world';
+console.log(dfv);
+
+let gh = "Showrov";
+let sk = "Programer";
+console.log(`${gh} is the best computer ${sk} in the world`);
