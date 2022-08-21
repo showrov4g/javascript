3e96f9b0749c2d11f6177320a8d7e567d2ec2743
showrov4g <showrovg00@gmail.com>
2022-08-21 12:09:43 +0600

    partice set

--- variable.js
+++ variable.js
@@ -1,79 +1,9 @@
-// const a = "হরে কৃষ্ণ হরে কৃষ্ণ, কৃষ্ণ কৃষ্ণ, হরে হরে, হরে রাম হরে রাম, রাম রাম হরে হরে  ";
-// console.log (a);
-
-// let sum = 0;
-// let n = prompt("inter the valu of n");
-// n = Number.parseInt(n);
-
-
-// for (let i = 0; i < n; i++){
-//     sum += (i+1);
-// }
-// console.log("sum of first" +" "+ n + "natural number is" + sum );
-// let obj = {
-//     showrov : 100,
-//     Bapi: 30,
-//     Apu: 0,
-//     Anando: 20,
-//     sk: 100,
-// };
-
-// for(let a in obj){
-//     console.log("mark of "+ a + " are "+ obj[a]);
-// }
-// let text = "showrov  ghosh";
-// let i = 0;
-// while (i < 10) {
-//   text += "</br> The number is " + i;
-//   i++;
-// }
-// console.log(text);
-
-// whole loop start ==========================================================================
-
-// let n = prompt("inter the value of n");
-// n = Number.parseInt(n);
-// let i = 0;
-// do{
-//     console.log(i);
-//     i++
-// }while(i<n);
-
-// let n = prompt("inter the value of n ");
-// n = Number.parseInt(n);
-// let i = 0;
-// do{
-//     console.log(i);
-//     i++
-// }while(i < n);
-// fucntion start 
-
-// function one_plus_avg(x, y){
-//     console.log("done");
-//     return Math.round(1 + (x + y)/2);
-// }
-
-// let a = 1;
-// let b = 2;
-// let c = 3;
-// console.log("one plush average of a and b is", one_plus_avg(a , b));
-// console.log("one plush average of b and b is", one_plus_avg(b,c));
-// console.log("one plush average of a and c is", one_plus_avg(c,a) );
-
-function one_plus_avg(x, y){
-    return Math.round(1 +( x + y)/2);
+let marks = {
+    showrov: 100,
+    dipu: 80,
+    Topu: 80,
+    apu: 95,
 }
-
-const sum = (p,q)=>{
-    return p + q;
+for(let i = 0; i<Object.keys(marks).length; i++){
+    
 }
-
-let a = 1;
-let b = 2;
-let c = 3;
-console.log("One plus average of a and b is", one_plus_avg(a, b) );
-console.log("One plus average of b and c is", one_plus_avg(b, c) );
-console.log("One plus average of b and a is", one_plus_avg(b, a) );
-console.log("One plus average of a and c is", one_plus_avg(a, c) );
-
-console.log(sum(9 ,7));
