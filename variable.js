9bcc46bba8e63cf7db669059cb0d1a1f741dc5bf
showrov4g <showrovg00@gmail.com>
2022-08-22 09:38:38 +0600

    21 agoust

--- variable.js
+++ variable.js
@@ -1,9 +1,26 @@
-let marks = {
-    showrov: 100,
-    dipu: 80,
-    Topu: 80,
-    apu: 95,
+// let marks = {
+//     showrov: 100,
+//     dipu: 80,
+//     Topu: 80,
+//     apu: 95,
+// }
+// // for(let i = 0; i<Object.keys(marks).length; i++){
+// //     console.log("This mark of" + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+// // }
+
+// for (let key in marks){
+//     console.log("the mark of" + key + " are " + marks[key]);
+// }
+
+// let a = 40;
+// let i;
+// while (i != a) {
+//     console.log("Try again");
+//     i = prompt("inter a number");
+// }
+// console.log("you have inter the write number");
+
+const mean = (a , b, c, d)=>{
+    return( a + b + c + d)/4;
 }
-for(let i = 0; i<Object.keys(marks).length; i++){
-    
-}
+console.log(mean(4,5,6,7))
