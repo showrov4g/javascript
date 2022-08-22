251f4b4c9a3819d5a7a857313657779d554a4afe
showrov4g <showrovg00@gmail.com>
2022-08-22 09:32:24 +0600

    22 agoust

--- variable.js
+++ variable.js
@@ -1,9 +1,17 @@
-let sbc = "Showrov ghosh";
-console.log(sbc);
+// // strign method in javascript
 
-let dfv = 'i am showrov ghosh. one of the best computer in the world';
-console.log(dfv);
+// let fgh = "shree showrov kumar ghosh";
+// console.log(fgh.substring(1, 5));
+// console.log(fgh.slice(-10, -5));
+// console.log(fgh.replace("shree","Programer"));
+// console.log(fgh.toLocaleUpperCase());
+// console.log(fgh.toLocaleLowerCase());
 
-let gh = "Showrov";
-let sk = "Programer";
-console.log(`${gh} is the best computer ${sk} in the world`);
+// let text1= "showrov";
+// let text2= "ghosh";
+// console.log(text1.concat(" ", text2));
+// let gh = "            showrov            ";
+// console.log(gh.trim());
+let text= "hello world";
+let pad = text.split("/");
+console.log(pad);
