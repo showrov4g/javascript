a772f8ca49fe715f28d88ce92917a4622285f88c
showrov4g <showrovg00@gmail.com>
2022-08-22 11:06:18 +0600

    string

--- variable.js
+++ variable.js
@@ -12,6 +12,39 @@
 // console.log(text1.concat(" ", text2));
 // let gh = "            showrov            ";
 // console.log(gh.trim());
-let text= "hello world";
-let pad = text.split("/");
-console.log(pad);
+// let text= "hello world";
+// let pad = text.split("/");
+// console.log(pad);
+let str = "sk\"";
+console.log(str.length);
+
+const sentace = 'the brown fox jump over the lazy dog';
+const word = "fox2";
+console.log(sentace.includes(word));
+console.log(`the word "${word}" ${sentace.includes(word) ? 'is': 'not'} in the sentace`);
+
+
+// p2 
+
+let ghosh = "showrov";
+let abc = ghosh.toUpperCase();
+let cba = ghosh.toLowerCase();
+let dg = ghosh.toLocaleLowerCase();
+let fgd = ghosh.toLocaleUpperCase();
+console.log(abc);
+console.log(cba);
+console.log(dg);
+console.log(fgd);
+
+// problem 4
+
+let dh = "Please give me 20000 taka";
+let amount = Number.parseInt(dh.slice(15));
+console.log(amount);
+console.log(typeof(amount));
+
+//problem
+
+let friend = "showrov";
+friend[3] = "R";
+console.log(friend);
