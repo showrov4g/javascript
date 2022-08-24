57728d3f6c474feaa6c592cd14a8c27ed352c462
showrov4g <showrovg00@gmail.com>
2022-08-24 11:10:44 +0600

    array

--- array.js
+++ array.js
@@ -1,11 +1,23 @@
-let num = [1, 2, 34, 4];
+// let num = [1, 2, 34, 4];
+// let b = num.toString();
+// console.log(b);
+// let c =num.join(" ro ");
+// console.log(c, typeof(c));
+// console.log(typeof(num));
+// let r = num.shift();
+// console.log(num, r); //remove the element
+
+// let f = num.unshift(777);
+// console.log(num, f);
+
+
+let num = [1, 2, 3, 4, 5, 6];
 let b = num.toString();
 console.log(b);
-let c =num.join(" ro ");
+let c = num.join(" ro ");
 console.log(c, typeof(c));
 console.log(typeof(num));
 let r = num.shift();
 console.log(num, r); //remove the element
-
-let f = num.unshift(777);
+let f = num.unshift(999);
 console.log(num, f);
