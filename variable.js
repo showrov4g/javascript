62ea81b3a1087b00b175b4d7ce0ca688fbfd0241
showrov4g <showrovg00@gmail.com>
2022-08-24 09:52:28 +0600

    start

--- variable.js
+++ variable.js
@@ -1,50 +1,53 @@
-// // strign method in javascript
+// // // strign method in javascript
 
-// let fgh = "shree showrov kumar ghosh";
-// console.log(fgh.substring(1, 5));
-// console.log(fgh.slice(-10, -5));
-// console.log(fgh.replace("shree","Programer"));
-// console.log(fgh.toLocaleUpperCase());
-// console.log(fgh.toLocaleLowerCase());
+// // let fgh = "shree showrov kumar ghosh";
+// // console.log(fgh.substring(1, 5));
+// // console.log(fgh.slice(-10, -5));
+// // console.log(fgh.replace("shree","Programer"));
+// // console.log(fgh.toLocaleUpperCase());
+// // console.log(fgh.toLocaleLowerCase());
 
-// let text1= "showrov";
-// let text2= "ghosh";
-// console.log(text1.concat(" ", text2));
-// let gh = "            showrov            ";
-// console.log(gh.trim());
-// let text= "hello world";
-// let pad = text.split("/");
-// console.log(pad);
-let str = "sk\"";
-console.log(str.length);
+// // let text1= "showrov";
+// // let text2= "ghosh";
+// // console.log(text1.concat(" ", text2));
+// // let gh = "            showrov            ";
+// // console.log(gh.trim());
+// // let text= "hello world";
+// // let pad = text.split("/");
+// // console.log(pad);
+// let str = "sk\"";
+// console.log(str.length);
 
-const sentace = 'the brown fox jump over the lazy dog';
-const word = "fox2";
-console.log(sentace.includes(word));
-console.log(`the word "${word}" ${sentace.includes(word) ? 'is': 'not'} in the sentace`);
+// const sentace = 'the brown fox jump over the lazy dog';
+// const word = "fox2";
+// console.log(sentace.includes(word));
+// console.log(`the word "${word}" ${sentace.includes(word) ? 'is': 'not'} in the sentace`);
 
 
-// p2 
+// // p2 
 
-let ghosh = "showrov";
-let abc = ghosh.toUpperCase();
-let cba = ghosh.toLowerCase();
-let dg = ghosh.toLocaleLowerCase();
-let fgd = ghosh.toLocaleUpperCase();
-console.log(abc);
-console.log(cba);
-console.log(dg);
-console.log(fgd);
+// let ghosh = "showrov";
+// let abc = ghosh.toUpperCase();
+// let cba = ghosh.toLowerCase();
+// let dg = ghosh.toLocaleLowerCase();
+// let fgd = ghosh.toLocaleUpperCase();
+// console.log(abc);
+// console.log(cba);
+// console.log(dg);
+// console.log(fgd);
 
-// problem 4
+// // problem 4
 
-let dh = "Please give me 20000 taka";
-let amount = Number.parseInt(dh.slice(15));
-console.log(amount);
-console.log(typeof(amount));
+// let dh = "Please give me 20000 taka";
+// let amount = Number.parseInt(dh.slice(15));
+// console.log(amount);
+// console.log(typeof(amount));
 
-//problem
+// //problem
 
-let friend = "showrov";
-friend[3] = "R";
-console.log(friend);
+// let friend = "showrov";
+// friend[3] = "R";
+// console.log(friend);
+
+
+// array learning
