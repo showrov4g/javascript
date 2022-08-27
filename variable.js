f142f1171289f3daa30e0e506c2274ce1b3bc24e
showrov4g <showrovg00@gmail.com>
2022-08-27 22:32:33 +0600

    element

--- variable.js
+++ variable.js
@@ -1,4 +1,11 @@
-let num = [1, 2, 3, 4, 5];
-for (let i=0; i<num.lenght; i++){
-    console.log(num[i]);
-}
+// let sk ="I am showrov ghosh";
+// console.log(sk);
+
+let num = [34, 56, 65, 34, 67, 87];
+// for(let i=0; i<num.length; i++){
+//     console.log(num);
+// }
+
+num.forEach((Element)=>{
+    console.log(Element*Element);
+})
