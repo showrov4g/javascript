85224edfc6bfbc307505950ed2c594e8b658531b
showrov4g <showrovg00@gmail.com>
2022-09-06 21:31:44 +0600

    Image

--- index.js
+++
@@ -1,6 +0,0 @@
-// const x = document.querySelectorAll("p.intro");
-// document.getElementById("demo").innerHTML = 
-// 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
-const x = document.querySelectorAll("p.intro");
-
-document.getElementById("demo").innerHTML = 'The first paragraph (index 0 ) with class = "intro" is:' + x[0].innerHTML;
