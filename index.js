baf7e6183edd9a0ea9801bf7603053d95995e993
showrov4g <showrovg00@gmail.com>
2022-09-06 21:11:41 +0600

    start

---
+++ index.js
@@ -0,0 +1,6 @@
+// const x = document.querySelectorAll("p.intro");
+// document.getElementById("demo").innerHTML = 
+// 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+const x = document.querySelectorAll("p.intro");
+
+document.getElementById("demo").innerHTML = 'The first paragraph (index 0 ) with class = "intro" is:' + x[0].innerHTML;
