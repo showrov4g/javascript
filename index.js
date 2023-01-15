4669b0d0bbbec0de999851786bfd7ba50d44a8a2
showrov4g <showrovg00@gmail.com>
2023-01-15 20:41:09 +0600

    area

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let a = 3;
-let b = 4;
-// document.write(a);
-document.write(a /=b);
+// area calculation 
+let base = prompt("inter the base=");
+let height = prompt("inter the height value = ");
+
+let area = base * height;
+
+document.write("area ="+ area);
