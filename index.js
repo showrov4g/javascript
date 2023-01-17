1dea0ec79e4a22a26ffa061cd5248e69d1a96e8c
showrov4g <showrovg00@gmail.com>
2023-01-17 21:23:25 +0600

    relation

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// let cels = parseFloat(prompt("enter Farenheight value"));
-
-// var farn = (cels*(9/5))+32;
-
-// document.write("Farenheight=" + farn);
 
 let cels = parseFloat(prompt("enter Celcious value"));
 
