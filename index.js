a9d4288dcd5ce7afe1b95cd834b21f791a9ad71e
showrov4g <showrovg00@gmail.com>
2023-01-18 21:15:51 +0600

    if

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log(20!=30);
+console.log(80>=70);
+
+// if (new Date().getHours(10) > 18) {
+//     document.getElementById("demo").innerHTML = "Good day!";
+//   }
+
+// const hour = new Date().getHours(20); 
+// let greeting;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// document.getElementById("demo").innerHTML = "greeting";
+
+let hour = new Date().getHours(20);
+
+let greeting;
+
+if(hour < 24){
+    greeting = "good Day";
+}else{
+    greeting = "Good Evenging";
+}
+
+document.getElementById("demo").innerHTML = greeting;
