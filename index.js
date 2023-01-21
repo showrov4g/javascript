416b56b8649ca04c850fc49a562ea71437e9b907
showrov4g <showrovg00@gmail.com>
2023-01-21 20:59:22 +0600

    better

---
+++ index.js
@@ -0,0 +1,11 @@
+let num = prompt("input your number");
+
+if(num => 100){
+    document.write("good");
+}else if(num => 80){
+    document.write("better")
+}else if (num => 50){
+    document.write("best");
+}else{
+    document.write("fail")
+}
