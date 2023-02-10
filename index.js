8db1213214d10f320d73962e0ec8f0479f3728bf
showrov4g <showrovg00@gmail.com>
2023-02-10 23:44:11 +0600

    javascript

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let num = prompt("inter the name candidate");
+let marks = prompt("Inter your number");
 
-if(num => "showrov"){
-    document.write("Showrov ghosh is One of the best programmer in this")
-}else if(num => "dipu"){
-    document.write("he is good boy");
-}else if (num => 'topu'){
-    document.write("he is very intelegent");
+if(marks >= 80 && marks <= 100) {
+    document.getElementById("demo").innerHTML = "A+"
+}else if(marks >= 70 && marks <= 79) {
+    document.getElementById("demo").innerHTML = "A"
+}else if(marks >= 60 && marks <= 69) {
+    document.getElementById("demo").innerHTML = "B"
+}else if(marks >= 50 && marks <= 59) {
+    document.getElementById("demo").innerHTML = "c"
+}else if(marks >= 40 && marks <= 49) {
+    document.getElementById("demo").innerHTML = "D"
+}else{
+    document.getElementById("demo").innerHTML = "Fail";
 }
