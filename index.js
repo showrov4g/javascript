c74f12220a3f9b9618bd1cd82338789f9d7dc410
showrov4g <showrovg00@gmail.com>
2023-02-22 15:06:28 +0600

    blank things

--- index.js
+++ index.js
@@ -27,27 +27,20 @@
 // console.log(day);
 
 
-let day;
+let text;
 switch(new Date().getDay()){
-        case 0:
-        day = "Sunday";
+    case 6:
+        text = "Today is Saturday";
         break;
-    case 1:
-        day = "Monday";
+      case 0:
+        text = "Today is Sunday";
         break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case  6:
-        day = "Saturday";
+      default:
+        text = "Looking forward to the Weekend";
+
 }
-console.log(day);
+console.log(text);
+document.write(text);
+
+
+to day in this video we will make Jarive assistan for your self. so let begain StaticRange.
