ab830f7505cd2ffd428966c3a5e23c93adb1e29d
showrov4g <showrovg00@gmail.com>
2024-08-06 16:58:56 +0600

    if condition

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,8 @@
-if(false){
-    console.log("executed")
+let country = "India";
+let age = 19 ;
+
+if (age >= 18 && country == "India"){
+    console.log("You got your Identity Card");
+}else{
+    console.log("You are not Eligible");
 }
