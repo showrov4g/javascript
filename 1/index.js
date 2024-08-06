9f6ed13e41b88eb813debd13e8f7037d01e1726c
showrov4g <showrovg00@gmail.com>
2024-08-06 17:05:37 +0600

    learnign comditions

--- 1/index.js
+++ 1/index.js
@@ -1,8 +1,12 @@
 let country = "India";
 let age = 19 ;
 
-if (age >= 18 && country == "India"){
+if (age >= 18){
     console.log("You got your Identity Card");
-}else{
+}
+else if(age >= 16){
+    console.log("you are a teenager")
+}
+else{
     console.log("You are not Eligible");
 }
