14370d77568251d16f09062f192d3b1071452fe0
showrov4g <showrovg00@gmail.com>
2024-08-08 22:42:58 +0600

    tarinary operator

--- 1/index.js
+++ 1/index.js
@@ -1,31 +1,5 @@
-let dayName = 3;
+let age = 20 ;
 
-switch (dayName) {
-    case 1:
-        dayName = "Sunday";
-        break;
-    case 2:
-        dayName = "Monday";
-        break;
-    case 3:
-        dayName = "tuesday";
-        break;
-    case 4:
-        dayName = "Wednesday";
-        break;
-    case 5:
-        dayName = "Tuesday";
-        break;
-    case 6:
-        dayName = "Friday";
-        break;
-    case 7:
-        dayName = "Saturday";
-        break;
+let mess = (age >= 20) ? "You are adult" : "You are miner";
 
-    default: 
-        dayName = "not a valid day";
-        break;
-}
-
-console.log("this day is: " + dayName);
+console.log(mess);
