d9751598ccf9433c12e578d9403f2c9611ca6f2e
showrov4g <showrovg00@gmail.com>
2024-08-12 22:09:40 +0600

    learning object method model

---
+++ 1/index.js
@@ -0,0 +1,9 @@
+const person = {
+    firsName: "eloin",
+    lastName: "mask",
+    greet: function greet(){
+        console.log("hello world");
+    }
+}
+person.greet();
+console.log(typeof person)
