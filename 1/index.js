a5f15146aa4587f3c276aa517e83660a03c150bd
showrov4g <showrovg00@gmail.com>
2024-08-13 10:47:51 +0600

    js prototype

--- 1/index.js
+++ 1/index.js
@@ -3,7 +3,9 @@ function Person(fName, lName) {
         this.lastName = lName
 }
 
-Person.prototype.gender = "male";
+Person.prototype.getFullName = function(){
+    return this.firstName + " " + this.lastName;
+}
 
 const Person1 = new Person("Elon", "musk");
 const Person2 = new Person("Bill", "Gates");
