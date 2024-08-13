7c815a533de5f268a8238f10fac58b73262c4b69
showrov4g <showrovg00@gmail.com>
2024-08-13 11:25:50 +0600

    js class method

--- 1/index.js
+++ 1/index.js
@@ -1,4 +1,16 @@
 
+class person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const person1 = new person("sk ghosh", 24);
+const person2 = new person("Dipu ghosh", 24);
+
+console.log(person2)
+
 
 
 
