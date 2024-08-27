3a52384c7b3c1287210a2e94a334607e1a85d39c
showrov4g <showrovg00@gmail.com>
2024-08-27 22:46:40 +0600

    learing js function

--- 1/index.js
+++ 1/index.js
@@ -1,4 +1,16 @@
-let btn = document.getElementById("btn");
-btn.onclick= function (){
-    console.log("button Clicked")
+
+function myFunction(p1, p2){
+    return p1 * p2;
 }
+
+let result = myFunction (4, 3);
+document.getElementById("demo").innerHTML = result
+
+
+// function myFunction(p1, p2) {
+//   return p1 * p2;
+// }
+
+// let result = myFunction(4, 3);
+// document.getElementById("demo").innerHTML = result;
+
