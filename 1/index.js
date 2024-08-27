2d595e8688c31ff37dd1cd32b2c5c44b5a3baacf
showrov4g <showrovg00@gmail.com>
2024-08-27 23:48:06 +0600

    learing js function

--- 1/index.js
+++ 1/index.js
@@ -1,16 +1,9 @@
+let text = "the temperature is" + toCelsius(77) + "Celsius";
 
-function myFunction(p1, p2){
-    return p1 * p2;
+document.getElementById("demo")
+.innerHTML = text;
+
+
+function toCelsius (fahrenheit){
+    return (5/9) * (fahrenheit-32);
 }
-
-let result = myFunction (4, 3);
-document.getElementById("demo").innerHTML = result
-
-
-// function myFunction(p1, p2) {
-//   return p1 * p2;
-// }
-
-// let result = myFunction(4, 3);
-// document.getElementById("demo").innerHTML = result;
-
