9a897ad60d2194dcc81f074144005dbdc16ed959
showrov4g <showrovg00@gmail.com>
2024-08-28 11:53:04 +0600

    learing js function

--- 1/index.js
+++ 1/index.js
@@ -1,9 +1,10 @@
-// learning js object  
-const person = {
-    firstName: "showrov",
-    lastName: "doe",
-    age: 50,
-    eyeColor: "blue"
+function person(first, last, age, eye){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
 }
+const myFather = new person("john", "Deo", 50, "blue");
+const myMother = new person ("Dony", "danail", 35, "blue");
 
-document.getElementById("demo").innerHTML = person.firstName + " is " + person.age + " years old";
+document.getElementById("demo").innerHTML = "My father is" + " " + myFather.age + "."+ "My mother name is"+ " "+ myMother.age ;
