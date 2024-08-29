cb3836b832377768495901fc2b50a3685ec3067f
showrov4g <showrovg00@gmail.com>
2024-08-30 00:47:15 +0600

    learning javasecript

--- js_condition/index.js
+++ js_condition/index.js
@@ -1,11 +1,11 @@
-// condition in js 
-let vote;
-let age = "raew";
+const salary = 25000;
+const isBCS = true;
+const hasCar = false;
+const height = 60;
 
-if (isNaN(age)) {
-    console.log("Input is not a number");
-    
+if((salary > 25000 && hasCar == true) || isBCS == true ){
+    console.log("everyone is Happy")
 }else{
-    console.log("you ar abole to vote");
+    console.log("everyone is not happy");
     
 }
