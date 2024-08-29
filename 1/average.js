1e3ed02a606d56b48be1a5ef2382210c71654b01
showrov4g <showrovg00@gmail.com>
2024-08-29 10:59:44 +0600

    average mark

---
+++ 1/average.js
@@ -0,0 +1,13 @@
+
+
+var Mathematics = 75.25;
+var Biology =  65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var average = (Mathematics + Biology + Chemistry + Physics + Bangla) / 5;
+
+console.log(average.toFixed(2));
+
+
