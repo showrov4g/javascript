18f9f3cd4980bf11c12891383d3170ea2d0bcacf
showrov4g <showrovg00@gmail.com>
2024-08-30 19:20:34 +0600

    bmi colcolotor maker

---
+++ js_condition/drink.js
@@ -0,0 +1,8 @@
+let drinks = 400;
+
+if (drinks >= 500) {
+    console.log("free cake");
+}else{
+    console.log("cake is 30 taka");
+    
+}
---
+++ js_condition/tarnary.js
@@ -0,0 +1,17 @@
+// let age = 19;
+
+// age > 18 ? console.log("you can vote") : console.log("go to home");
+
+let price = 500;
+
+const isLeader = true;
+
+// if (isLeader === true){
+//     price = 0;
+// }
+// else {
+//     price = price + 100;
+// }
+// console.log(price)
+
+price = isLeader === true ? 0 : price + 100;
