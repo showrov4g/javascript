f8f119a171e4339a1e22934a8f53d7e460618bb6
showrov4g <showrovg00@gmail.com>
2024-09-04 00:43:54 +0600

    learning javaScript

---
+++ looping/loop.js
@@ -0,0 +1,11 @@
+const friends = ["Showrov", "Dipu", "Toup","Apu"];
+
+for (const friend of friends){
+    // console.log(friend);
+}
+
+
+for (let i = 0; i < 10; i++){
+    console.log(i);
+    
+}
