61bdbdab02fa0652d47f18a7cd2b3ac0d2421bbc
showrov4g <showrovg00@gmail.com>
2024-09-03 12:12:17 +0600

    learng js

--- object/slice.js
+++ object/slice.js
@@ -11,8 +11,45 @@ const car = {
     make: "Toyota",
     model: "Corolla",
     year: 2020,
-    passenger: {
-        capacity: 5
-    }
 };
-console.log(car.passenger.capacity);
+car.passenger= {
+    capacity: 5
+}
+
+// console.log(car);
+// console.log(car.passenger.capacity);
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics);
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let property = Object.keys(student).length;
+
+// console.log(property);
+
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+for (let key in myObject) {
+    if (myObject.hasOwnProperty(key)) {
+        let value = myObject[key];
+        console.log(`${key} ||| ${value} Type: ${typeof value}`);
+    }
+}
+
