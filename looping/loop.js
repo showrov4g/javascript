63ed3b90b025528a650cfc0fcd0efdbc2d6ad754
showrov4g <showrovg00@gmail.com>
2024-09-04 01:06:16 +0600

    learning js

--- looping/loop.js
+++ looping/loop.js
@@ -1,11 +1,27 @@
 const friends = ["Showrov", "Dipu", "Toup","Apu"];
 
+
+
+
 for (const friend of friends){
     // console.log(friend);
 }
 
 
-for (let i = 0; i < 10; i++){
-    console.log(i);
+// for (let i = 0; i < friends.length; i++){
+//     console.log(i);
+//     console.log(friends[i]);
     
+// }
+
+
+const number = [234,54564,6,6567,7,678,879,776,878,78,78,78979 ,78,878,7 ,87,];
+
+// for (let i = 0; i <number.length; i++){
+//     console.log(number[i])
+// }
+let i = 0;
+while (i < number.length){
+    console.log(number[i]);
+    i++;
 }
