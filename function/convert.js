2ca64d7314ec7ff8cfc7307f9ca3fcc7aa019a47
showrov4g <showrovg00@gmail.com>
2024-09-06 23:51:55 +0600

    making mile to kilometer conveter

--- function/convert.js
+++ function/convert.js
@@ -1,8 +1,29 @@
-function inchToFeet(){
-   const feet = inch / 12;
-   return feet;   
+// function inchToFeet(inch){
+//    const feet = inch / 12;
+//    return feet;   
+// }
+
+// const showrovHeight = inchToFeet(70);
+
+// console.log(showrovHeight);
+
+
+// function inchToFeet2(inch){
+//     const feetFraction = inch / 12;
+//     const feetNumber = parseInt(feetFraction);
+//     const inchRemaining = inch % 12;
+//     const result = feetNumber + " Ft " + inchRemaining + " inch "
+//     return result;
+// }
+
+// const showrovHeight = inchToFeet2(75);
+// console.log(showrovHeight);
+
+// mile to kilometer 
+
+function mileToKilometer(mile){
+    const Kilo = mile * 1.60934;
+    return Kilo;
 }
-
-const showrovHeight = inchToFeet();
-
-console.log(showrovHeight);
+const kilometer =  mileToKilometer(50);
+console.log(kilometer)
