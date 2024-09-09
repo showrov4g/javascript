d36d40041a43065eb8a5c3a71745948387db4c10
showrov4g <showrovg00@gmail.com>
2024-09-09 09:40:37 +0600

    making a calculator{

---
+++ problem_js/calculator.js
@@ -0,0 +1,39 @@
+// making a calculator
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// calculator function 
+
+function calculator(a, b, operation) {
+    if (operation == "add") {
+        const result = add(a, b);
+        return result;
+    }
+    else if (operation === "subtract") {
+        const result = subtract(a, b);
+        return result;
+    } else if (operation === "multiply") {
+        return multiply(a, b);
+    }
+    else if (operation === 'divide') {
+        return divide(a, b);
+    } else {
+        return "Operation is not allowed";
+    }
+}
+
+const result = calculator(5,7, "multiply");
+console.log(result);
--- problem_js/layerdiscount.js
+++ problem_js/layerdiscount.js
@@ -4,25 +4,32 @@
 3. above 200 ---> 70;
 */
 
-function layerDiscountTotal (quantity){
+function layerDiscountTotal(quantity) {
     const first100Price = 100;
     const second100Price = 90;
     const above200Price = 70;
-    
-    if (quantity <= 100){
+
+    if (quantity <= 100) {
         const total = quantity * first100Price;
         return total;
     }
-    else if( quantity <= 200){
-        const total = quantity * second100Price;
+    else if (quantity <= 200) {
+        const first100total = 100 * first100Price;
+        const remainingQuantity = quantity - 100;
+        const remainingTotal = remainingQuantity * second100Price;
+        const total = first100total + remainingTotal
         return total;
     }
-    else{
-        const total = quantity * above200Price;
+    else {
+        const first100T0Total = 100 * first100Price;
+        const second100Total = 100 * second100Price;
+        const remainingQuantity = quantity - 200;
+        const remainingTotal = remainingQuantity * above200Price;
+        const total = first100T0Total + second100Total + remainingTotal;
         return total;
     }
 }
 
-const total = layerDiscountTotal(300);
+const total = layerDiscountTotal(1000);
 
 console.log(total);
