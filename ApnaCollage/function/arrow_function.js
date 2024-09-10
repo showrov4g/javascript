0ae0d44a584de3467c0bfa900966e3290407da76
showrov4g <showrovg00@gmail.com>
2024-09-10 09:04:53 +0600

    learning javascript function

---
+++ ApnaCollage/function/arrow_function.js
@@ -0,0 +1,13 @@
+let arrow= (a,b)=>{
+    console.log(a+b);
+}
+
+// arrow(4,65);
+
+// multiplay with arrow function 
+
+let multi = (a,b)=>{
+    console.log(a*b);
+}
+
+multi(5,6);
--- ApnaCollage/function/start.js
+++ ApnaCollage/function/start.js
@@ -1,17 +1,27 @@
-function myFunction(){
+function myFunction() {
     console.log("This is showrov ghsoh");
     console.log("learnign javaScript");
-    
+
 };
 
 // geting message throw function 
 
-function message(msg){
+function message(msg) {
     return msg;
-    
+
 }
 
 
 let finalMessage = message("this is ghosh. A expert javaScript developer");
 
-console.log(finalMessage);
+// console.log(finalMessage);
+// making a calculation function 
+
+
+function sum(a, b) {
+    let result = a + b;
+    return result;
+}
+
+let result = sum(2, 4);
+console.log(result);
