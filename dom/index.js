99b4a17954fdd9aa470e1f047630864dc2defcb5
showrov4g <showrovg00@gmail.com>
2024-09-13 18:28:22 +0600

    learnign js dom

--- dom/index.js
+++ dom/index.js
@@ -1,3 +1,10 @@
-let head = document.getElementById("heading");
+let h2 = document.querySelector('h2');
+console.log(h2.innerText + "this is ghosh an expert computer programmer.")
 
-console.dir(head);
+let divs = document.getElementsByClassName('text')
+
+let idx = 1;
+for(let div of divs){
+    div.innerText = `this is ghosh from ${idx}`;
+    idx++;
+}
