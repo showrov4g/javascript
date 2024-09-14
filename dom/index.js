92fca3e0b2da57b326845db15286674aa814a422
showrov4g <showrovg00@gmail.com>
2024-09-14 17:01:40 +0600

    learing javaScript dom

--- dom/index.js
+++ dom/index.js
@@ -1,3 +1,13 @@
+
+
+// let newButton = document.createElement("button");
+
+// newButton.innerText = "click me!";
+// let div = document.querySelector('p');
+// div.after(newButton);
+
+
+let heading = document.createElement('h1');
+heading.innerText = "This is JavaScript heading";
 let div = document.querySelector('div');
-div.style.backgroundColor = 'green';
-div.style.visibility ="hidden";
+div.after(heading)
