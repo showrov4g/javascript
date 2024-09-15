81247a77c85bf76b0deb55934f8f383431528075
showrov4g <showrovg00@gmail.com>
2024-09-15 23:11:04 +0600

    js dom

--- dom/domEvent/index.js
+++ dom/domEvent/index.js
@@ -1,2 +1,20 @@
 // console.log("this is a seperate javaScript file");
 
+// function yellow(){
+//     document.body.style.background = 'yellow';
+// }
+
+// function green(){
+//     document.body.style.background = "green";
+// }
+
+const blue = document.getElementById('blue');
+blue.onclick = function red(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+const change =  document.getElementById('click');
+
+change.onclick = function makeYellow(){
+   document.body.style.backgroundColor = 'yellow';
+}
