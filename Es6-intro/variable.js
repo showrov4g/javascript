f6df2ac14d70d7b665cc622f222953d05886a7da
showrov4g <showrovg00@gmail.com>
2024-09-25 22:03:22 +0600

    learning js

---
+++ Es6-intro/variable.js
@@ -0,0 +1,17 @@
+// let num = 50;
+//  num = 100;
+
+// console.log(num);
+
+// const man = 50;
+
+// const rich = man + 100;
+
+// console.log(rich);
+
+let sum = 0;
+
+for (let i = 1; i < 100; i++) {
+  total = sum + i;
+}
+console.log(total);
