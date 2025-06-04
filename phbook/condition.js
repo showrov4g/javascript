e1f32ab63cbf3b4e0795cbf2e7b2e41c8f900598
showrov4g <showrovg00@gmail.com>
2025-06-04 10:07:06 +0600

    learnign javascript

---
+++ phbook/condition.js
@@ -0,0 +1,24 @@
+// const money = 500;
+// const popcornPrice= 500;
+
+// if (money >= 300){
+//     console.log("Going to watch a movie");
+//     if(popcornPrice <= 50){
+//         console.log("Buying PopCron")
+//     }else 
+//     {
+//         console.log("PopCron is Expensive")
+//     }
+
+// }else{
+//     console.log("not going to move")
+// }
+
+
+let invite = false;
+
+if(invite == true){
+    console.log("I will go")
+}else{
+    console.log("I will unfriend my friend")
+}
